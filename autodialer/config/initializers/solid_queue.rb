58c192b07c42c1a91627eaeb6a223dc44daad468
Srinath K <srinath262003@gmail.com>
2025-11-06 07:06:12 +0530

    deleted

--- autodialer/config/initializers/solid_queue.rb
+++
@@ -1,6 +0,0 @@
-# config/initializers/solid_queue.rb
-# Force SolidQueue to use the primary ActiveRecord database in production
-
-if defined?(SolidQueue)
-  SolidQueue::Record.connects_to database: { writing: :primary }
-end
